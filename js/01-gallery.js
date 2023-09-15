573dcc3da5ed883cbd74f69a25e0af46bd46c86a
Nazarii-Ivan Stosyk <fantombeowulf@gmail.com>
2023-09-15 20:47:45 +0300

    hw7 task1 is done (without "Escape" button)

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,33 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
+const listEl = document.querySelector('.gallery');
+const renderList = (arr,container) => {
+    const markup =  arr.map(
+        (item) =>
+            `<li class="gallery__item">
+            <a class="gallery__link href="${item.original}">
+            <img
+            class="gallery__image"
+            src="${item.preview}"
+            alt="${item.description}"
+            data-source="${item.original}" />
+            </a>
+            </li>`
+    ).join("");
+    container.insertAdjacentHTML('beforeend', markup);
+}
+renderList(galleryItems, listEl);
 
+listEl.addEventListener('click', (event) => {
+    event.preventDefault();
+    if (event.currentTarget == event.target) {
+        return;
+    }
+    const instance = basicLightbox.create(`
+    <div class="modal">
+    <img src="${event.target.dataset.source}">
+    </div>
+`)
+    instance.show()
+});
 console.log(galleryItems);
